3c5ba17c020c5413d7192fd88f6b8d21fae58f34
jreynard-code <jeremy.reynard@cosmotech.com>
2026-09-01 10:58:46 +0200

    Add error static pages for common error codes
    - org.springframework.boot:spring-boot-starter-thymeleaf is now in dependencies
    - minimal error static pages defined
    - internationalization files added

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,6 +45,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
   implementation("org.springframework.boot:spring-boot-starter-actuator")
   implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webflux")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
